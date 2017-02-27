7534bfb0d8bd8e9bc72180dc081e5d1ddb889a6e
8bitMaverick <8bitmaverick@gmail.com>
2017-02-27 03:08:40 -0800

    BarcodeIO complete, ask prof about public/same file issue

--- src/Foothill.java
+++ src/Foothill.java
@@ -34,7 +34,15 @@ public class Foothill
    }   
 }
 
-
+interface BarcodeIO
+{
+   public boolean scan(BarcodeImage bc);
+   public boolean readText(String text);
+   public boolean generateImageFromText();
+   public boolean translateImageToText();
+   public void displayTextToConsole();
+   public void displayImageToConsole();
+}
 
 class BarcodeImage implements Cloneable
 {
